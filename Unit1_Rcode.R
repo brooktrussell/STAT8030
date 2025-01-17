4c9e607f99b2450824676d8132959252ece03c88
brooktrussell <66636617+brooktrussell@users.noreply.github.com>
2025-01-17 09:29:06 -0500

    Update Unit1_Rcode.R

--- Unit1_Rcode.R
+++ Unit1_Rcode.R
@@ -34,3 +34,31 @@ quantile(boot_vec,c(.025,.975))
 library(boot)
 boot_out <- boot(salaries,function(dat,inds){mean(dat[inds])},R=10000)
 boot.ci(boot_out)
+
+
+
+#Miss America problem
+recent.grp <- c(19.5,20.3,19.6,20.2,17.8,17.9,19.1,18.8,17.6,16.8)
+past.grp <- c(20.4,21.9,22.1,22.3,20.3,18.8,18.9,19.4,18.4,19.1)
+
+mean(recent.grp);sd(recent.grp)
+mean(past.grp);sd(past.grp)
+
+#test on variances, then pooled or non-pooled test
+var.test(recent.grp,past.grp)#FTR
+t.test(recent.grp,past.grp,alt="less",var.equal=TRUE)
+
+#permutation test on difference of means
+choose(20,10)#number of possible permutations
+new.dat <- c(recent.grp,past.grp)
+obs.mean.diff <- mean(recent.grp) - mean(past.grp)
+nsim <- 100000
+sim.mean.diff <- rep(NA,length=nsim)
+for (i in 1:nsim){
+  grps <- sample(c(rep(1,10),rep(2,10)),replace=FALSE)
+  sim.mean.diff[i] <- mean(new.dat[grps==1]) - mean(new.dat[grps==2])
+}
+
+hist(sim.mean.diff);abline(v=obs.mean.diff,col="red",lty=2)
+length(sim.mean.diff[sim.mean.diff<=obs.mean.diff])/nsim #estimated p-value
+
